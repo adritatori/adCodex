7bd9f2d72d07c19951e7e4481d957852eb437dc4
adritatori <adritatori456@gmail.com>
2023-03-08 18:46:06 +0600

    updatee

--- server/server.js
+++ server/server.js
@@ -23,18 +23,11 @@ const app = express();
 
 // app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://ad-codex-a7n82y34q-adritatori.vercel.app/");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+})
 
 app.use(express.json());
 app.get('/', async (req, res) => {

328f1008084695aa1241868649c99c820d5c55d5
adritatori <adritatori456@gmail.com>
2023-03-08 19:11:24 +0600

    See if it works

--- server/server.js
+++ server/server.js
@@ -21,8 +21,12 @@ const app = express();
 
 // app.use(cors(corsOptions));
 
-app.use(cors());
-
+// app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://ad-codex-a7n82y34q-adritatori.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
